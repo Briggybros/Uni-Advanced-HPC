01f1620199283d0677d8692a72ddeeeea2a0b075
Gregory Sims <briggybros.2@googlemail.com>
2018-02-15 15:01:05 +0000

    Define variables

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -122,26 +122,33 @@ void usage(const char* exe);
 ** initialise, timestep loop, finalise
 */
 int main(int argc, char* argv[]) {
-  char*    paramfile = NULL;    /* name of the input parameter file */
-  char*    obstaclefile = NULL; /* name of a the input obstacle file */
-  t_param  params;              /* struct to hold parameter values */
-  t_speed* cells     = NULL;    /* grid containing fluid densities */
-  t_speed* tmp_cells = NULL;    /* scratch space */
-  int*     obstacles = NULL;    /* grid indicating which cells are blocked */
-  float* av_vels   = NULL;      /* a record of the av. velocity computed for each timestep */
-  struct timeval timstr;        /* structure to hold elapsed time */
-  struct rusage ru;             /* structure to hold CPU time--system and user */
-  double tic, toc;              /* floating point numbers to calculate elapsed wallclock time */
-  double usrtim;                /* floating point number to record elapsed user CPU time */
-  double systim;                /* floating point number to record elapsed system CPU time */
+  // char*    paramfile = NULL;    /* name of the input parameter file */
+  // char*    obstaclefile = NULL; /* name of a the input obstacle file */
+  // t_param  params;              /* struct to hold parameter values */
+  // t_speed* cells     = NULL;    /* grid containing fluid densities */
+  // t_speed* tmp_cells = NULL;    /* scratch space */
+  // int*     obstacles = NULL;    /* grid indicating which cells are blocked */
+  // float* av_vels   = NULL;      /* a record of the av. velocity computed for each timestep */
+  // struct timeval timstr;        /* structure to hold elapsed time */
+  // struct rusage ru;             /* structure to hold CPU time--system and user */
+  // double tic, toc;              /* floating point numbers to calculate elapsed wallclock time */
+  // double usrtim;                /* floating point number to record elapsed user CPU time */
+  // double systim;                /* floating point number to record elapsed system CPU time */
 
   /* parse the command line */
-  if (argc != 3) {
-    usage(argv[0]);
-  } else {
-    paramfile = argv[1];
-    obstaclefile = argv[2];
-  }
+  // if (argc != 3) {
+  //   usage(argv[0]);
+  // } else {
+  //   paramfile = argv[1];
+  //   obstaclefile = argv[2];
+  // }
+
+  int rank;               /* 'rank' of process among it's cohort */ 
+  int size;               /* size of cohort, i.e. num processes started */
+  int flag;               /* for checking whether MPI_Init() has been called */
+  int strlen;             /* length of a character array */
+  enum bool {FALSE,TRUE}; /* enumerated type: false = 0, true = 1 */  
+  char hostname[MPI_MAX_PROCESSOR_NAME];  /* character array to hold hostname running process */
 
   MPI_Init(argc, argv);
   MPI_Initialized(&flag);
